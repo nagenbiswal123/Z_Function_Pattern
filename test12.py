ca1adb18a7ba31d75fad872dc749d840bd15d9cf
Nagen Biswal <104751344+nagenbiswal123@users.noreply.github.com>
2024-04-22 17:07:47 +0000

    new changes

--- test12.py
+++ test12.py
@@ -2,6 +2,8 @@
 #Always write a variable name in proper syntax/proper name, otherwise Interviewer will think you don't have the real Experience.
 #Ruleno1: Always declare a variable in small case
 #If you variable name in 2 part like it below, EC2_Instance_Name
+# and also we are clalled snake casing , Ec2_Instance_Name
+#In Camle casing format to write a varibale name is, Ec2InstanceName
 
 a=9
 b=3
